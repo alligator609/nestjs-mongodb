4557189a77497206e8c0a1470364a74759d824d3
Zia Uddin Ahmed <zia.ete10@gmail.com>
2022-10-03 22:18:39 +0600

    gloabl validation added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // use that to filter out not added data 
+    }),
+  ); // global validation
   const config = new DocumentBuilder()
     .setTitle('Nest With Mongo')
     .setDescription('API description')
--- src/user/dtos/new-user.dto.ts
+++ src/user/dtos/new-user.dto.ts
@@ -1,14 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty } from "class-validator";
 
 export class NewUserDTO {
   @ApiProperty()
+  @IsNotEmpty()
   name: string;
   @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
   @ApiProperty()
+  @IsNotEmpty()
   password: string;
   @ApiProperty()
+  @IsNotEmpty()
   phone: string;
   @ApiProperty()
+  @IsNotEmpty()
   userType: string;
 }
